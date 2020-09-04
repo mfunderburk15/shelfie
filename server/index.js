0d6d4eb63b5250eea2a931e05a200e727a409d59
masen.funderburk@gmail.com <masen.funderburk@gmail.com>
2020-09-04 12:39:49 -0500

    db setup

--- server/index.js
+++ server/index.js
@@ -1,10 +1,24 @@
 const express = require('express')
+const massive = require('massive')
 const shelfCtrl = require('./controller')
+require('dotenv').config()
+
+const { SERVER_PORT, CONNECTION_STRING } = process.env
 
 const app = express()
+app.use(express.json())
 
-const SERVER_PORT = 3000;
 
-app.listen(SERVER_PORT, () => {
-    console.log(`This app is using port:${SERVER_PORT}`)
+massive({
+    connectionString: CONNECTION_STRING,
+    ssl: {
+        rejectUnauthorized: false
+    },
+}).then(db => {
+    app.set('db', db)
+    console.log('db connected')
+    app.listen(SERVER_PORT, () => {
+        console.log(`This app is using port:${SERVER_PORT}`)
+    })
 })
+
