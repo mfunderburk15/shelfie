c29719da49704d95b474b107922bcb770fe381c1
masen.funderburk@gmail.com <masen.funderburk@gmail.com>
2020-09-04 22:34:05 -0500

    finished logic of part1 FIXED INFINITE LOOP BUG

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,57 +1,67 @@
-import React, { Component } from 'react'
-import Axios from 'axios'
+import React, { Component } from 'react';
+import axios from 'axios';
 
-export default class Form extends Component {
-    constructor() {
-        super()
+class Form extends Component {
+    constructor(props) {
+        super(props);
         this.state = {
             name: '',
             price: 0,
-            imgurl: '',
+            imgurl: ''
         }
-        this.handleChange = this.handleChange.bind(this)
-        this.onCancel = this.onCancel.bind(this)
     }
 
-    handleChange(evt) {
-        this.setState({
-            [evt.target.name]: evt.target.value
-        })
+    handleImage(imgurl) {
+        this.setState({ imgurl: imgurl })
+    }
+
+    nameInput(name) {
+        this.setState({ name: name })
+    }
+
+    handlePrice(price) {
+        this.setState({ price: price })
+    }
+
+    handleSubmit() {
+        let { name, price, imgurl } = this.state;
+        let product = {
+            name, price, imgurl,
+        }
+        axios.post('/api/product', product)
+            .then(res => {
+                this.props.getInventory();
+                this.onCancel();
+            })
+            .catch(err => console.log('There was an error creating this product', err))
     }
 
     onCancel() {
         this.setState({
             name: '',
             price: 0,
-            imgurl: '',
+            imgurl: ''
         })
     }
 
 
-
     render() {
-        const { name, price, imgurl } = this.state
-
         return (
-            <div>
-                <input
-                    value={imgurl}
-                    name='imgurl'
-                    onChange={this.handleChange}
-                />
-                <input
-                    value={name}
-                    name='name'
-                    onChange={this.handleChange}
-                />
-                <input
-                    value={price}
-                    name='price'
-                    onChange={this.handleChange}
-                />
-                <button onClick={this.onCancel}>Cancel</button>
-                <button>Add To Inventory</button>
+            <div className='Form'>
+                <img src={this.state.imgurl} />
+                <p>Image URL:</p>
+                <input type='text' value={this.state.imgurl} onChange={e => this.handleImage(e.target.value)} />
+                <p>Product Name:</p>
+                <input type='text' value={this.state.name} onChange={e => this.nameInput(e.target.value)} />
+                <p>Price:</p>
+                <input type='text' pattern="[0-9]*" value={this.state.price} onChange={e => this.handlePrice(e.target.value)} />
+                <div className='form_button_box'>
+                    <button onClick={() => this.onCancel()}>Cancel</button>
+                    <button onClick={() => this.handleSubmit()}>Add to Inventory</button>
+                </div>
             </div>
-        )
+        );
     }
 }
+
+export default Form;
