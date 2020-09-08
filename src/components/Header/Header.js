6e2ac746e4a1ae596450589a69f10ef1e773e757
masen.funderburk@gmail.com <masen.funderburk@gmail.com>
2020-09-08 13:18:44 -0500

    header finished

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,11 +4,12 @@ import { Link } from 'react-router-dom';
 
 export default function Header() {
     return (
-        <div className='Header'>
+        <div className='header'>
+            <img className='icon' src='https://shelfie.devmountain.com/v2/part3/favicon.ico' />
             <h1>Shelfie</h1>
-            <div className='header_link_box'>
-                <Link to='/'>Dashboard</Link>
-                <Link to='/add'>Add Inventory</Link>
+            <div className='header_nav'>
+                <Link className='link' to='/'>Dashboard</Link>
+                <Link className='link' to='/add'>Add Inventory</Link>
             </div>
         </div>
     )
--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { HashRouter as Router } from 'react-router-dom'
+import './reset.css'
 
 ReactDOM.render(
   <Router>
