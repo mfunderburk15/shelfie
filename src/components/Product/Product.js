3735c871bdfb3fa8c0cf50f80df35b77f4404594
masen.funderburk@gmail.com <masen.funderburk@gmail.com>
2020-09-08 15:39:27 -0500

    dashboard/product styling finished

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -4,15 +4,15 @@ import { withRouter } from 'react-router-dom'
 function Product(props) {
     let { id, name, price, imgurl } = props.data;
     return (
-        <div className='Product'>
-            <img src={imgurl} />
-            <div className='product_box'>
-                <p className='product_title'>{name}</p>
-                <p className='product_price'>${price}</p>
+        <div className='product'>
+            <img className='product-img' src={imgurl} />
+            <div className='product-info'>
+                <p className='product-name'>{name}</p>
+                <p className='product-price'>${price}</p>
             </div>
-            <div className='product_button_box'>
-                <button onClick={() => props.deleteProduct(id)}>Delete</button>
-                <button onClick={() => props.history.push(`/edit/${props.data.id}`)}>Edit</button>
+            <div className='product-control'>
+                <button className='product-button' onClick={() => props.deleteProduct(id)}>Delete</button>
+                <button className='product-button' onClick={() => props.history.push(`/edit/${props.data.id}`)}>Edit</button>
             </div>
         </div>
     )
